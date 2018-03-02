5363c972ab0dbc793dfd0654ea77e805a6af18bb
Mateusz Mazurek <mateusz.mazurek@gmail.com>
2018-03-02 12:27:55 +0100

    add saga todo

--- src/Solidry/Aspects/Saga/WithSaga.cs
+++ src/Solidry/Aspects/Saga/WithSaga.cs
@@ -7,6 +7,8 @@ namespace Solidry.Aspects.Saga
     /// </summary>
     public class WithSaga<TInput, TOutput>: IObservable<SagaEvent<TInput, TOutput>>
     {
+        //TODO Iobservable is out maybe better is just register right Observer i.e. Observer<SagaStartProcessing
+
         public IDisposable Subscribe(IObserver<SagaEvent<TInput, TOutput>> observer)
         {
             throw new NotImplementedException();
