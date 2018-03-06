130757528f95f2493d01d24184e68ef06a9659b7
Mateusz Mazurek <mateusz.mazurek@gmail.com>
2018-03-06 11:14:48 +0100

    start implementing min for structs

--- src/Solidry/Extensions/Analytics.cs
+++ src/Solidry/Extensions/Analytics.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using Solidry.Helpers;
 using Solidry.Results.Analytics;
 
@@ -15,7 +16,7 @@ namespace Solidry.Extensions
         /// <param name="array">Source</param>
         /// <param name="capacity">Number of minimal elements</param>
         /// <returns></returns>
-        public static MinResult<T> Min<T>(this T[] array, int capacity = 1) where T : IComparable<T>
+        public static MinResult<T> Min<T>(this T[] array, int capacity = 1) where T : class, IComparable<T>
         {
             if (capacity < 1)
             {
@@ -52,6 +53,47 @@ namespace Solidry.Extensions
 
             return new MinResult<T>(min, rest);
         }
+
+        //TODO: memory copy
+        public static MinResult<T> _Min<T>(this T[] array, int capacity = 1) where T : struct, IComparable<T> 
+        {
+//            array._Min()
+            if (capacity < 1)
+            {
+                throw new InvalidOperationException($"Argument {nameof(capacity)} has to be greater than 0.");
+            }
+
+            if (capacity > array.Length)
+            {
+                throw new InvalidOperationException($"Argument {nameof(capacity)} has to be equal or less than collection.");
+            }
+
+            var min = new T[capacity];
+            var rest = new T[array.Length - capacity];
+
+            Array.Copy(array, min, capacity);
+            Array.Copy(array, capacity, rest, 0, array.Length - capacity);
+            Array.Sort(min);
+
+            int lastIndex = capacity - 1;
+
+            for (int i = 0; i < rest.Length; i++)
+            {
+                T e = rest[i];
+
+                int c = e.CompareTo(min[lastIndex]);
+
+                if (c < 0)
+                {
+                    UtilsHelper.Swap(ref rest[i], ref min[lastIndex]);
+
+                    min.SwapRightUntil((left, right) => left.CompareTo(right) > -1);
+                }
+            }
+
+            return new MinResult<T>(min, rest);
+        }
+
 
         //TODO: add Max<T>
 
